f5e546cad660667299822a52175042ca98d762c8
Satyam <satyamgarde30@gmail.com>
2024-04-06 22:46:44 +0530

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1>
+        Netflix clone using React.js
+      </h1>
+      
+    </>
+  )
+}
+
+export default App
