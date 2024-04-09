a2146d9e56a5d65fe2940217c72a0e8d63bacb11
Satyam <satyamgarde30@gmail.com>
2024-04-09 13:50:39 +0530

    created custom hook

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React from "react";
+import Header from "./Header";
+import useNowPlayingMovies from "./Hooks/useNowPlayingMovies";
+import Maincontainer from "./BrowseContainer/Main/Maincontainer";
+import { SecondaryContainer } from "./BrowseContainer/Secondary/SecondaryContainer";
 const Browse = () => {
-  return (
-    <div>Browse</div>
-  )
-}
 
-export default Browse
+  useNowPlayingMovies();
+
+  return (
+    <>
+      <Header />
+      <Maincontainer/>
+      <SecondaryContainer/>
+      <div className="text-black z-20 mt-9">Browse</div>
+     
+    </>
+  );
+};
+
+export default Browse;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import { LOGO_URL } from '../Utils/Constants'
 const Header = () => {
   return (
     <div>
-      <img className='absolute z-10 w-44 px-8 py-2 bg-gradient-to-b from-black' src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png' alt='Logo'></img>
+      <img className='absolute z-20 w-44 px-8 py-2 bg-gradient-to-b from-black' src={LOGO_URL} alt='Logo'></img>
     </div>
   )
 }
